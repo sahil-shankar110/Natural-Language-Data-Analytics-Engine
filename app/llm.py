c051e14264eee115abb96c049cf9c785cdc90c3d
Sahil Shankar <sahilshankar431@gmail.com>
2026-08-17 10:08:03 +0500

    Switch LLM implementation to ChatGoogleGenerativeAI
    Replaced ChatGroq with ChatGoogleGenerativeAI and updated API key usage.

--- app/llm.py
+++ app/llm.py
@@ -1,4 +1,5 @@
-from langchain_groq import ChatGroq
+# from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
 from app.prompts import sql_prompt
 from langchain_core.output_parsers import JsonOutputParser
 from dotenv import load_dotenv
@@ -7,13 +8,19 @@ import os
 load_dotenv()
 
 
-groq_api_key = os.getenv("GROQ_API_KEY")
-llm = ChatGroq(
-    api_key= groq_api_key,
-    model="meta-llama/llama-4-scout-17b-16e-instruct",
-    temperature=0.4,
-    max_tokens=None,
-    max_retries=2)
+# groq_api_key = os.getenv("GROQ_API_KEY")
+OOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+# llm = ChatGroq(
+#     api_key= groq_api_key,
+#     model="meta-llama/llama-4-scout-17b-16e-instruct",
+#     temperature=0.4,
+#     max_tokens=None,
+#     max_retries=2)
+llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash-lite",
+                             google_api_key=GOOGLE_API_KEY,
+                             temperature=0.4,
+                             max_tokens=None,
+                             max_retries=2)
     
 chain =  sql_prompt| llm | JsonOutputParser() 
 
